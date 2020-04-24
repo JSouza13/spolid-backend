0fa85ee77fc28a758ba4c9be6092d0aa342935c1
Joao Vitor <jooao_souza@hotmail.com>
2020-04-23 23:20:38 -0300

    config banco

--- src/config/database.js
+++ src/config/database.js
@@ -15,13 +15,14 @@ module.exports = {
     },
   },
   production: {
-    dialect: 'postgres',
-    host: process.env.DB_HOST,
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
     operatorsAliases: false,
-    use_env_variable: process.env.DATABASE_URL,
+    use_env_variable: 'DATABASE_URL',
     dialectOptions: {
       ssl: true,
     },
