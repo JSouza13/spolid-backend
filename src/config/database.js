0ca547be584d1a8bf21bc38deb1d8526a3b839d6
João Vitor <jooao_souza@hotmail.com>
2020-05-18 15:17:15 -0300

    Correção: Layout e-mail e variável depreciada database

--- src/config/database.js
+++ src/config/database.js
@@ -7,8 +7,6 @@ module.exports = {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    operatorsAliases: false,
-
     define: {
       timestamps: true,
       underscored: true,
@@ -22,7 +20,6 @@ module.exports = {
     host: process.env.DB_HOST,
     dialect: 'postgres',
     use_env_variable: 'DATABASE_URL',
-    operatorsAliases: false,
     sslmode: require,
     define: {
       timestamps: true,
