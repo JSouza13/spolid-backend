892ffebdf933f39c55211eeec96ba44940b67482
João Vitor <jooao_souza@hotmail.com>
2020-05-17 22:44:28 -0300

    Correção: alteração de DB production

--- src/config/database.js
+++ src/config/database.js
@@ -14,13 +14,11 @@ module.exports = {
     },
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
     dialect: 'postgres',
-    use_env_variable: 'DATABASE_URL',
+    connectionString: 'DATABASE_URL',
+    ssl: {
+      rejectUnauthorized: false,
+    },
     define: {
       timestamps: true,
       underscored: true,
