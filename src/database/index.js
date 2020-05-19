5d245467aff1f478e590dc50f78d29c971dfed8b
João Vitor <jooao_souza@hotmail.com>
2020-05-18 22:53:54 -0300

    Novo: Models de Matricula e Aulas assistidas

--- src/database/index.js
+++ src/database/index.js
@@ -1,12 +1,14 @@
 import Sequelize from 'sequelize';
 
+import Assisted_class from '../app/models/Assisted_class';
 import Class from '../app/models/Class';
 import Course from '../app/models/Course';
+import Enrollment from '../app/models/Enrollment';
 import File from '../app/models/File';
 import User from '../app/models/User';
 import databaseConfig from '../config/database';
 
-const models = [User, File, Course, Class];
+const models = [User, File, Course, Class, Assisted_class, Enrollment];
 
 class Database {
   constructor() {
